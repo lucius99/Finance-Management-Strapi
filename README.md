9a51eb9cfbba350acf45f36fc2f13a40bd473fde
lucius99 <47803507+lucius99@users.noreply.github.com>
2021-07-04 17:24:29 +0700

    [ADD] add a transaction API

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Strapi application
 
-A quick description of your strapi application
+API: 
+
+- POST /transactions
+body: 
+{
+    "money": 12000,
+    "currencyType": "vnd",
+    "isIncome": true,
+    "description": "test",
+    "category": "60e177a093661316dc484641",
+    "users_permissions_user": "60e17f644e02081c943ce9ac",
+    "custom_category": null
+}
--- api/transactions/controllers/transactions.js
+++ api/transactions/controllers/transactions.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.transactions.create(data, { files });
+    } else {
+      entity = await strapi.services.transactions.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.transactions });
+  },
+};
