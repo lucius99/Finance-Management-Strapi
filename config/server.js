ff0aba32fa951f6c9bc3093d76278a9f8feaf91a
lucius99 <47803507+lucius99@users.noreply.github.com>
2021-07-04 15:22:44 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3343340324306b3cdb014ae74750e898'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '07203cbf-6dbd-47bd-900a-66b823c7e46d'
+};
